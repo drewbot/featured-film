89fa6ae30813cb6be6c67f8f9046f4acbbeded3a
Drew Botka <hellodrewbot@gmail.com>
2015-01-12 13:35:05 -0500

    styling update, spread out the spacing in the header

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 
-$(window).resize(function(){
-	if ($(window).width() <= 720){	
-		$('.circle').hide()
-	};
-    if ($(window).width() >= 720){	
-		$('.circle').show()
-	}	
-});
+// $(window).resize(function(){
+// 	if ($(window).width() <= 720){	
+// 		$('.circle').hide()
+// 	};
+//     if ($(window).width() >= 720){	
+// 		$('.circle').show()
+// 	}	
+// });
 
